3c5ba605e5ec6aea6085e7fef685d8bff485c419
NrdyBhu1@gmail.com <bunnu16aug>
2020-12-06 16:11:03 +0530

    Added Logic

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@ bool isNumber(std::string x){
     else return false;
 }
 
+bool endGame = false;
 std::string tictactoeArr[18] = {"1", "2", "3", "4", "5", "6", "7", "8", "9"};
 
 void updateBoard(){
@@ -22,7 +23,58 @@ void updateBoard(){
 	std::cout << " " << std::endl;
 }
 
+bool checkHor(){
+	if (tictactoeArr[0] == tictactoeArr[1] && tictactoeArr[1] == tictactoeArr[2]){
+		return true;
+	}else if (tictactoeArr[3] == tictactoeArr[4] && tictactoeArr[4] == tictactoeArr[5]){
+		return true;
+	}else if (tictactoeArr[6] == tictactoeArr[7] && tictactoeArr[7] == tictactoeArr[8]){
+		return true;
+	}else{
+		return false;
+	}
+}
+
+bool checkVer(){
+	if (tictactoeArr[0] == tictactoeArr[3] && tictactoeArr[3] == tictactoeArr[6]){
+		return true;
+	}else if (tictactoeArr[1] == tictactoeArr[4] && tictactoeArr[4] == tictactoeArr[7]){
+		return true;
+	}else if (tictactoeArr[2] == tictactoeArr[5] && tictactoeArr[5] == tictactoeArr[8]){
+		return true;
+	}else{
+		return false, "none";
+	}
+}
+
+bool checkDiagonals(){
+	if (tictactoeArr[0] == tictactoeArr[4] && tictactoeArr[4] == tictactoeArr[8]){
+		return true;
+	}else if (tictactoeArr[2] == tictactoeArr[4] && tictactoeArr[4] == tictactoeArr[6]){
+		return true;
+	}else{
+		return false;
+	}
+}
+
+void checkWinners(){
+	bool Diag = checkDiagonals();
+	bool Hor = checkHor();
+	bool Ver = checkVer();
+	if(Diag && !Hor && !Ver){
+		endGame = true;
+		std::cout << YELLOW << tictactoeArr[4]+" is the winner" << RESET << std::endl;
+	}else if(!Diag && Hor && !Ver){
+		endGame = true;
+		std::cout << YELLOW << tictactoeArr[4]+" is the winner" << RESET << std::endl;
+	}else if(!Diag && !Hor && Ver){
+		endGame = true;
+		std::cout << YELLOW << tictactoeArr[4]+" is the winner" << RESET << std::endl;
+	}
+}
+
 void processCmd(std::string comd){
+	checkWinners();
 	if (isNumber(comd)){
 		int nm = std::atoi(comd.c_str());
 		if(nm < 10 && tictactoeArr[nm-1] != "X" && tictactoeArr[nm-1] != "O"){
@@ -33,7 +85,9 @@ void processCmd(std::string comd){
 			std::cout << RED << "Please enter a valid number" << RESET << std::endl;
 		}
 	}else{
-		if (comd != "end"){
+		if (comd == "end"){
+			endGame = true;
+		}else{
 			updateBoard();
 			std::cout << RED << "Please enter a valid number" << RESET << std::endl;
 		}
@@ -48,7 +102,7 @@ int main(int argc, char** argv) {
 
 	//Code
 	std::string cmd = "";
-	while (cmd != "end"){
+	while (!endGame){
 		std::cout << ">:";
 		std::cin >> cmd;
 		processCmd(cmd);
