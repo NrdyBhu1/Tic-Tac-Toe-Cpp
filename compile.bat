38f8408cf5dca122d55b0e6776b8e0c210dfa29a
NrdyBhu1@gmail.com <bunnu16aug>
2020-12-06 15:36:02 +0530

    Added a dependence for icon.rc.o , Update README.md

--- compile.bat
+++ compile.bat
@@ -1,5 +1,11 @@
 @echo off
+
+IF NOT EXIST icon.res.o (
+    windres icon.rc -o icon.res.o
+)
+
 g++ main.cpp -std=c++11 icon.res.o -o TicTacToe.exe
+
 IF EXIST TicTacToe.exe (
     mkdir build
     move TicTacToe.exe build/
